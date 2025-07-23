520fb127e50c003cd7e730f663ada6bc785e88b8
Alejandro Moscoso Deossa <alejo-mdm@hotmail.com>
2025-07-22 23:19:04 -0500

    I change the font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+        
         <link rel="icon" href="/favicon.png" type="image/png" />
       </Head>
       <body className="antialiased">
